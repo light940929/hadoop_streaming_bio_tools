5bd7a2572fd0bfb41786a27b6d6e203c24ee6b55
linhanjen <linhanjen@gmail.com>
2013-09-10 21:15:54 +0800

    test

---
+++ lib/BioTools/sam/library.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+#coding :utf-8
+
+module BioTools
+    module SAM
+      module Library
+        #IMPORTANT NOTE: Windows library is missing in this distribution
+
+        # Return the path with the file name of the library for the specific operating system
+        def filename
+          #TODO refactor this piece of code in all the files
+          lib_os = case RUBY_PLATFORM
+          when /linux/
+            'so.1'
+          when /darwin/
+            '1.dylib'
+          when /windows/
+            'dll'
+          else
+            case RUBY_DESCRIPTION
+            when /jruby.*darwin/
+              '1.dylib'
+            when /jruby.*linux/
+            'so.1'
+            end
+          end
+
+          File.join(File.expand_path(File.dirname(__FILE__)),'external',"libbam.#{lib_os}")
+        end #filename
+        module_function :filename
+      end #Library
+    end #Sam
+end #Bio
---
+++ lib/tools.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+#coding :utf-8
+
+require 'rubygems'
+require 'ffi'
+require 'BioTools/bwa'
+require 'BioTools/sam'
+require 'BioTools/pileup'
+require 'BioTools/vcf'
